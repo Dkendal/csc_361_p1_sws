a5c23d9eea2b1650dce20594566368a726c0395a
Dylan Kendal <dkendal@freerunningtechnologies.com>
2014-10-08 19:09:12 -0700

    swapped test params around to make output more readable

--- tests/http_server_test.cc
+++ tests/http_server_test.cc
@@ -9,112 +9,109 @@ class HttpServerTest : public ::testing::Test
     HttpServer *server;
 };
 class RequestTest : public HttpServerTest { };
-class IsResourceValidTest : public HttpServerTest { };
+class IsResourceReadableTest : public HttpServerTest { };
 
-// IsResourceValidTest
-TEST_F(IsResourceValidTest, ValidFile)
+// IsResourceReadableTest
+TEST_F(IsResourceReadableTest, ValidPath)
 {
-  EXPECT_TRUE(server->IsResourceValid("/index.html"));
-  EXPECT_TRUE(server->IsResourceValid("/"));
-  EXPECT_TRUE(server->IsResourceValid("/gnu/main.html"));
+  // at the root
+  EXPECT_TRUE(server->IsResourceReadable("/index.html"));
+  // in a subdir
+  EXPECT_TRUE(server->IsResourceReadable("/gnu/main.html"));
 }
 
-TEST_F(IsResourceValidTest, InvalidFile)
+TEST_F(IsResourceReadableTest, InvalidPath)
 {
-  EXPECT_FALSE(server->IsResourceValid("/cantRead"));
-  EXPECT_FALSE(server->IsResourceValid("/../"));
-  EXPECT_FALSE(server->IsResourceValid("/.."));
-  EXPECT_FALSE(server->IsResourceValid("/some_file.html"));
-  EXPECT_FALSE(server->IsResourceValid("http.request"));
-  EXPECT_FALSE(server->IsResourceValid("/gnu/"));
+  // insufficient permissions
+  EXPECT_FALSE(server->IsResourceReadable("/cantRead"));
+  // parent directory
+  EXPECT_FALSE(server->IsResourceReadable("/../"));
+  EXPECT_FALSE(server->IsResourceReadable("/.."));
+  // file in parent directory
+  EXPECT_FALSE(server->IsResourceReadable("/../Makefile"));
+  // non-existant file
+  EXPECT_FALSE(server->IsResourceReadable("/some_file.html"));
+  // file at root without '/'
+  EXPECT_FALSE(server->IsResourceReadable("http.request"));
+  // a directory
+  EXPECT_FALSE(server->IsResourceReadable("/gnu/"));
 }
-// end IsResourceValidTest
+// end IsResourceReadableTest
 
 TEST_F(RequestTest, Index)
 {
-  Response result = server->Request("GET /index.html HTTP/1.0");
-
-  EXPECT_STREQ(result.header.c_str(), "HTTP/1.0 200 OK");
+  EXPECT_EQ("HTTP/1.0 200 OK",
+      server->Request("GET /index.html HTTP/1.0").header);
 }
 
 TEST_F(RequestTest, Root)
 {
   Response result = server->Request("GET / HTTP/1.0");
 
-  EXPECT_STREQ(result.header.c_str(),"HTTP/1.0 200 OK");
+  EXPECT_EQ("HTTP/1.0 200 OK", result.header);
 }
 
 TEST_F(RequestTest, NestedResource)
 {
   Response result = server->Request("GET /gnu/main.html HTTP/1.0");
 
-  EXPECT_STREQ(result.header.c_str(),"HTTP/1.0 200 OK");
+  EXPECT_EQ("HTTP/1.0 200 OK", result.header);
 }
 
 TEST_F(RequestTest, NonexistantFile)
 {
-  Response result = server->Request("GET /nofile HTTP/1.0");
-
-  EXPECT_STREQ(result.header.c_str(),"HTTP/1.0 404 Not Found");
+  EXPECT_EQ("HTTP/1.0 404 Not Found", server->Request("GET /nofile HTTP/1.0").header);
 }
 
 TEST_F(RequestTest, NonexistantNestedFile)
 {
-  Response result = server->Request("GET /gnu/nofile HTTP/1.0");
-
-  EXPECT_STREQ(result.header.c_str(),"HTTP/1.0 404 Not Found");
+  EXPECT_EQ("HTTP/1.0 404 Not Found",
+      server->Request("GET /gnu/nofile HTTP/1.0").header);
 }
 TEST_F(RequestTest, VerbAndProtocolCaseInsensitive)
 {
-  Response result = server->Request("get / http/1.0");
-
-  EXPECT_STREQ(result.header.c_str(),"HTTP/1.0 200 OK");
+  EXPECT_EQ("HTTP/1.0 200 OK",
+   server->Request("get / http/1.0").header);
 }
 
 TEST_F(RequestTest, ResourceCaseSensitive)
 {
-  Response result = server->Request("GET /Index.html HTTP/1.0");
-
-  EXPECT_STREQ(result.header.c_str(),"HTTP/1.0 404 Not Found");
+  EXPECT_EQ("HTTP/1.0 404 Not Found",
+      server->Request("GET /Index.html HTTP/1.0").header);
 }
 
 TEST_F(RequestTest, InvalidURI)
 {
-  Response result = server->Request("GET http.request HTTP/1.0");
-
-  EXPECT_STREQ(result.header.c_str(),"HTTP/1.0 400 Bad Request");
+  EXPECT_EQ("HTTP/1.0 400 Bad Request",
+      server->Request("GET http.request HTTP/1.0").header);
 }
 
 TEST_F(RequestTest, UnreadableFile)
 {
-  Response result = server->Request("GET /cantRead HTTP/1.0");
-
-  EXPECT_STREQ(result.header.c_str(),"HTTP/1.0 404 Not Found");
+  EXPECT_EQ("HTTP/1.0 404 Not Found",
+      server->Request("GET /cantRead HTTP/1.0").header);
 }
 
 TEST_F(RequestTest, InvalidProtocol)
 {
-  EXPECT_STREQ( server->Request("GET /index.html HTTP/1.1").header.c_str(),
-      "HTTP/1.0 400 Bad Request");
+  EXPECT_EQ("HTTP/1.0 400 Bad Request",
+      server->Request("GET /index.html HTTP/1.1").header);
 }
 
 TEST_F(RequestTest, CantReadDirectory)
 {
-  Response result = server->Request("GET /gnu/ HTTP/1.1");
-
-  EXPECT_STREQ(result.header.c_str(),"HTTP/1.0 400 Bad Request");
+  EXPECT_EQ("HTTP/1.0 400 Bad Request",
+      server->Request("GET /gnu/ HTTP/1.1").header);
 }
 
 TEST_F(RequestTest, UnknownVerb)
 {
-  Response result = server->Request("got / http/1.0");
-
-  EXPECT_STREQ(result.header.c_str(),"HTTP/1.0 400 Bad Request");
+  EXPECT_EQ("HTTP/1.0 400 Bad Request",
+      server->Request("got / http/1.0").header);
 }
 
 TEST_F(RequestTest, ParentDirectory)
 {
-  Response result = server->Request("GET /../ HTTP/1.0");
-
-  EXPECT_STREQ(result.header.c_str(),"HTTP/1.0 404 Not Found");
+  EXPECT_EQ("HTTP/1.0 404 Not Found",
+      server->Request("GET /../ HTTP/1.0").header);
 }
